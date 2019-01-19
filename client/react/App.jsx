6352645ee55fb560d1417a221b56c215b6eb5f69
MidnightPineapple <samuel88835@yahoo.com>
2019-01-19 00:19:17 -0500

    added React-Ace editor

--- client/react/App.jsx
+++ client/react/App.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import styles from './App.css'
+import AceEditor from 'react-ace';
+
+import 'brace/mode/javascript';
+import 'brace/theme/twilight';
 
 import { IFrameParentConnection } from './lib'
 
 export default class App extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editorText:""
+        }
+    }
+
     componentDidMount() {
         this.frame = new IFrameParentConnection();
+    }   
+
+    editorOnChange(editorText, event) {
+        this.setState({ editorText });
+
+        // event { start,end:{line,column} lines:[ affected lines ] action:(insert|remove) }
     }
 
     render() {
@@ -14,6 +32,14 @@ export default class App extends Component {
         return (
             <div className="app" >
                 <p>Hello from React!</p>
+                <AceEditor
+                mode="javascript"
+                theme="twilight"
+                name="Editor Name Here"
+                height="6em"
+                value={this.state.editorText}
+                onChange = {this.editorOnChange.bind(this)}
+                />
                 <iframe src={GAME_IFRAME_URI} className={styles.frame} sandbox="allow-scripts allow-same-origin" />
             </div>
         )
