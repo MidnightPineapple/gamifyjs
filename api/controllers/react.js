cde0d872812e2fc2da38c4317cd35438b021434d
Shikhar <ShikharDixit@shikis-air.sbu.edu>
2019-01-25 13:08:35 -0500

    added authentication method

--- api/controllers/react.js
+++ api/controllers/react.js
@@ -10,15 +10,15 @@ module.exports = async function(req, res) {
         res.status(404);
         return res.send(JSON.stringify("Not Found"));
     }
-    
+
     try {
         let file = await readFile(INDEX_HTML_PATH, 'utf-8');
-        // make the path to react bundle from html-webpack-plugin an absolute path 
+        // make the path to react bundle from html-webpack-plugin an absolute path
         file = file.replace(/(<script src=")(assets\/js\/[\w.-_]+.bundle.js)/,'$1/$2');
         res.status(200);
         return res.send(file)
     } catch(e) {
         return res.notFound();
     }
-    
+
 }
