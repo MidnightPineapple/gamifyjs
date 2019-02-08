7f4942e6bf4014eb38be0bd1e16405a25d534959
MidnightPineapple <samuel88835@yahoo.com>
2019-02-08 06:04:12 -0500

    styled notfound page and corrected another file naming descrpancy

--- client/react/scenes/NotFoundScene/NotFoundScene.jsx
+++ client/react/scenes/NotFoundScene/NotFoundScene.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './NotFoundScene.css'
 
 export default class NotFoundScene extends Component {
 
     render() {
         return(
-            <div style={styles.content}>
-                <i class="fas fa-heart-broken"></i>
-
-                <h1>Oops we couldn't find the page you're looking for.</h1>
-                <Link to="/">Back To Homepage</Link>                
-            </div>
+            <header style={styles.header}>
+                <i className={"fas fa-heart-broken "+styles.icon}></i>
+                <h1 className={styles.title}>Oops we couldn't find that.</h1>
+                <Link className={styles.link} to="/">Back To Homepage</Link>                
+            </header>
         )
     }
 
