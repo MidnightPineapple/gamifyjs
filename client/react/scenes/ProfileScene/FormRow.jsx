7ba73ed3949e9f1b3c7bd07e20411866367a8473
MidnightPineapple <samuel88835@yahoo.com>
2019-04-12 06:27:22 -0400

    stubbed profile scene

---
+++ client/react/scenes/ProfileScene/FormRow.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import styles from './FormRow.css'
+
+export default ({ value, label, onChange, onSubmit }) =>{
+
+    const [ editing, setEditing ] = useState(false)
+    const [ input, setInput ] = useState("")
+
+    return (
+        <div className={styles.container}>
+            { editing 
+                ? <form onSubmit={(e) => { onSubmit(e); setEditing(false) }}>
+                    <label>{label} </label>
+                    <input type="text" value={input || value} onChange={e => setInput(e.target.value)} />
+                </form>
+                : <div onClick={() => setEditing(true)}>{label}: {value}</div>
+            }
+        </div>
+    )
+}
--- client/react/scenes/ProfileScene/ProfileScene.jsx
+++ client/react/scenes/ProfileScene/ProfileScene.jsx
@@ -1,10 +1,81 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import contain from '../../ContentContainer'
+import FormRow from './FormRow'
+import styles from './ProfileScene'
 
-const ProfileScene = () => (
-    <Fragment>
-        <h3>Welcome User!</h3>
-    </Fragment>
-)
+const user = {
+    firstName: "John",
+    lastName: "Smith",
+    email: "user@example.com",
+}
+
+class ProfileScene extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            form: {
+                firstName: {
+                    value: user.firstName,
+                    editing: false,
+                    label: "First Name",
+                    slug:'first-name'
+                },
+                lastName: {
+                    value: user.lastName,
+                    editing:false,
+                    label: "Last Name",
+                    slug: "last-name"
+                },
+                email: {
+                    value: user.email,
+                    editing: false,
+                    label: "Email",
+                    slug: 'email'
+                },
+                password: {
+                    value: "",
+                    editing: false,
+                    label: "Change Password",
+                    slug: 'password'
+                }
+            }
+        }
+    }
+
+    onSubmit(fieldName, event) {
+        event.preventDefault()
+        const form = Object.assign({}, this.state.form)
+        const field = Object.assign({}, this.state.form[fieldName])
+
+        field.value = event.target[0].value
+        form[fieldName] = field
+        this.setState({ form })
+
+        // TODO: make network request here
+    }
+
+
+    render() {
+
+        const { form } = this.state
+
+        return (
+            <Fragment>
+                <h3>User Profile: John Smith</h3>
+                {
+                    Object.keys(form).map( (v,k) => (
+                        <FormRow 
+                        key={v}
+                        value={form[v].value} 
+                        label={form[v].label} 
+                        onSubmit={e => this.onSubmit(v,e)} />
+                    ))
+                }
+            </Fragment>
+        )
+    }
+
+}
 
 export default contain(ProfileScene, { background:"#ddd", color:"#222" })
